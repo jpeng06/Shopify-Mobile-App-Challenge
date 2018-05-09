cf655d0320f7970a7377769de5b704339b009686
Jpeng <john.peng@ericsson.com>
2018-05-09 01:07:00 -0400

    init

---
+++ Shopify/OrderViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OrderViewController.swift
+//  Shopify
+//
+//  Created by ezpenju on 2018-05-06.
+//  Copyright © 2018 com.jpeng.go.shopify. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func loadData() {
+        
+        
+        
+    }
+
+
+}
+
